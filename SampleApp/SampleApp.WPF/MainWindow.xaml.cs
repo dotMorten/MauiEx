8f975a0780ca7624a5b741cd01d294a0a3c2f31f
Dominik Mydlil <dominik.mydlil@outlook.com>
2019-04-25 10:56:24 +0200

    Add WPF sample app

---
+++ SampleApp/SampleApp.WPF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace SampleApp.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            LoadApplication(new SampleApp.App());
+        }
+    }
+}
