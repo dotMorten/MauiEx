53e80e1f6541ccbfefef3daa6d5ab9bfaa729f06
Morten Nielsen <mn@iter.dk>
2023-10-18 23:04:20 -0700

    doc

--- AutoSuggestBox/AppHostBuilderExtensions.cs
+++ AutoSuggestBox/AppHostBuilderExtensions.cs
@@ -5,11 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace dotMorten.Maui
-{/// <summary>
- /// AppHost builder methods for registering the controls with .NET MAUI.
- /// </summary>
+{
+    /// <summary>
+    /// AppHost builder methods for registering the controls with .NET MAUI.
+    /// </summary>
     public static class AppHostBuilderExtensions
     {
+        /// <summary>
+        /// Initializes the AutoSuggestBox controls with Maui.
+        /// </summary>
+        /// <param name="builder">The Maui host builder.</param>
+        /// <returns>The host builder.</returns>
         public static MauiAppBuilder UseAutoSuggestBox(this MauiAppBuilder builder)
         {
             return builder.ConfigureMauiHandlers((a) => { a.AddHandler(typeof(AutoSuggestBox), typeof(Handlers.AutoSuggestBoxHandler)); });
