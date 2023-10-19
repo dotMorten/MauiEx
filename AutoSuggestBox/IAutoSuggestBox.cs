3f1944cea1fe4971f609fbec9817fc3f9ee0fb26
Morten Nielsen <mn@iter.dk>
2023-10-18 22:16:08 -0700

    Add missing doc

--- AutoSuggestBox/IAutoSuggestBox.cs
+++ AutoSuggestBox/IAutoSuggestBox.cs
@@ -6,20 +6,48 @@ using System.Threading.Tasks;
 
 namespace dotMorten.Maui
 {
+    /// <summary>
+    /// A set of AutoSuggestBox members used with the <see cref="Handlers.AutoSuggestBoxHandler"/> class.
+    /// </summary>
+    /// <remarks>
+    /// This API might expand in the future, and implementing it isn't recommended.
+    /// </remarks>
     public interface IAutoSuggestBox : IView
     {
+        /// <inheritdoc cref="AutoSuggestBox.Text"/>
         string Text { get; set; }
+
+        /// <inheritdoc cref="AutoSuggestBox.TextColor"/>
         Color TextColor { get; set; }
+
+        /// <inheritdoc cref="AutoSuggestBox.PlaceholderText"/>
         string PlaceholderText { get; set; }
+
+        /// <inheritdoc cref="AutoSuggestBox.PlaceholderTextColor"/>
         Color PlaceholderTextColor { get; set; }
+
+        /// <inheritdoc cref="AutoSuggestBox.TextMemberPath"/>
         string TextMemberPath { get; set; }
+
+        /// <inheritdoc cref="AutoSuggestBox.DisplayMemberPath"/>
         string DisplayMemberPath { get; set; }
+
+        /// <inheritdoc cref="AutoSuggestBox.IsSuggestionListOpen"/>
         bool IsSuggestionListOpen { get; set; }
+
+        /// <inheritdoc cref="AutoSuggestBox.UpdateTextOnSelect"/>
         bool UpdateTextOnSelect { get; set; }
+
+        /// <inheritdoc cref="AutoSuggestBox.ItemsSource"/>
         System.Collections.IList ItemsSource { get; set; }
-        
+
+        /// <inheritdoc cref="AutoSuggestBox.QuerySubmitted"/>
         void QuerySubmitted(string queryText, object chosenSuggestion);
+
+        /// <inheritdoc cref="AutoSuggestBox.TextChanged"/>
         void TextChanged(string text, AutoSuggestionBoxTextChangeReason reason);
+
+        /// <inheritdoc cref="AutoSuggestBox.SuggestionChosen"/>
         void SuggestionChosen(object selectedItem);
     }
 }
